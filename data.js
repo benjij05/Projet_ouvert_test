0e0917c2b668a58227d60a715f0b8b5beec759b9
benjij05 <102859814+benjij05@users.noreply.github.com>
2026-01-21 21:09:08 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+const catalogue = {
+    "apl-demande": {
+        titre: "Demander les APL",
+        description: "Tutoriel complet pour remplir votre dossier CAF en ligne sans erreurs.",
+        youtubeId: "jHeQFMSogEY", // Exemple: Si l'URL est youtube.com/watch?v=dQw4w9WgXcQ, l'ID est dQw4w9WgXcQ
+        duree: "4 min",
+        categorie: "Logement"
+    },
+    "hlm-inscription": {
+        titre: "Demander un logement social",
+        description: "Les étapes pour s'inscrire sur le fichier national des HLM.",
+        youtubeId: "autre_id_youtube",
+        duree: "5 min",
+        categorie: "Logement"
+    },
+    "compte-ameli": {
+        titre: "Créer son compte Ameli",
+        description: "Accédez à vos remboursements de santé en ligne.",
+        youtubeId: "id_video_sante",
+        duree: "3 min",
+        categorie: "Santé"
+    }
+};
